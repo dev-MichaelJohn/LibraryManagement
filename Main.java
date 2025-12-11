6f9c103a100ef1537799bd9d51a8c50ba07bf0d7
Michael John Larido <136241846+dev-MichaelJohn@users.noreply.github.com>
2025-12-11 21:07:12 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+//import myGUI.LibraryManager;
+import gui.LibraryManager;
+
+public class Main {
+    public static void main(String[] args) {
+        LibraryManager app = new LibraryManager();
+    }
+}
